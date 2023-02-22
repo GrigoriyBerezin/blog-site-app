1a09c82b3a2e601f287cf007649397a054bf3edf
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-22 21:42:45 +0300

    dell idea

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,21 @@
 import React from "react"
 import logo from "../../assets/images/logo.svg"
 import "./header.css"
-import { User } from "./User"
-import { nav } from "../../assets/data/data"
-import { Link } from "react-router-dom"
+import {User} from "./User"
+import {nav} from "../../assets/data/data"
+import {Link} from "react-router-dom"
 
 export const Header = () => {
-   window.addEventListener("scroll", function () {
+  window.addEventListener("scroll", function () {
     const header = this.document.querySelector(".header")
     header.classList.toggle("active", this.window.scrollY > 100)
-  }) 
+  })
   return (
     <>
       <header className='header'>
-        <div className='scontainer flex'>
+        <div className='container flex'>
           <div className='logo'>
-            <img src={logo} alt='logo' width='100px' />
+            <img src={logo} alt='logo' width='100px'/>
           </div>
           <nav>
             <ul>
@@ -27,7 +27,7 @@ export const Header = () => {
             </ul>
           </nav>
           <div className='account flexCenter'>
-            <User />
+            <User/>
           </div>
         </div>
       </header>
